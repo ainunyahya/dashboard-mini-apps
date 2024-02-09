4da3a0fdaba0b5f5646b90eba719da9dab86d45b
Yayee <ainunyahya12@gmail.com>
2024-02-09 11:36:00 +0700

    mengahapus komen

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -166,11 +166,7 @@
               </a>
             </li>
             <li class="nav-item px-1 d-flex align-items-center">
-              {{-- <a class="dropdown-item" href="{{ route('logout') }}"
-                  onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                  {{ __('Logout') }}
-              </a> --}}
+              
 
               <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                   @csrf
